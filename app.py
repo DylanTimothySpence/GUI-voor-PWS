c7b138773f639e03a93e6bf1af8693d0b15dd6d0
DylanTimothySpence <greatoneofthesea@gmail.com>
2024-12-20 22:02:43 +0100

    Raamwerk voldoende voor GUI

--- app.py
+++ app.py
@@ -7,13 +7,16 @@ app = Flask(__name__)
 def login(src):
     if request.method == "POST":
         srcbox = request.form.get("srcbox")
-        return redirect(url_for("map", src=srcbox))
+        dest_label = request.form.get("dest_label")
+        dest_number = request.form.get("dest_number")
+        print(dest_number)
+        return redirect(url_for("map", src=srcbox, dest_label=dest_label, dest_number=dest_number))
     else:
         return render_template("login.html", src=src)
 
-@app.route("/van-<src>")
-def map(src):
-    return render_template("map.html", src=src)
+@app.route("/van-<src>-naar-<dest_label>-<dest_number>")
+def map(src, dest_label, dest_number):
+    return render_template("map.html", src=src, dest_label=dest_label, dest_number=dest_number)
 
 if __name__ == "__main__":
     app.run(debug=True)
