df8fbdcfcabe45478f809dc59ff5a584f2fb73ec
DylanTimothySpence <greatoneofthesea@gmail.com>
2025-01-30 13:45:54 +0100

    type_tijd toegevoegd (nog geen effect op ETA)

--- tobias_app_v2.py
+++ tobias_app_v2.py
@@ -16,25 +16,25 @@ def login(src):
             srcbox_van = int(request.form.get("srcbox_van"))
             srcbox_naar = int(request.form.get("srcbox_naar"))
             dest_label = f"#{srcbox_naar}"
-            # print(request.form.get("time"))
+            # soort_tijd = int(request.form.get("typeTijd"))
             time_input = request.form.get("time")
-            print(f"time_input: {time_input}")
-            return redirect(url_for("map", src=srcbox_van, dest_label=dest_label, dest_number=srcbox_naar, speed=speed_slider, time_input=time_input))
+            type_tijd = request.form.get('typeTijd')
+            return redirect(url_for("map", src=srcbox_van, dest_label=dest_label, dest_number=srcbox_naar, speed=speed_slider, time_input=time_input, type_tijd=type_tijd))
         else:
             return render_template("login_tobias.html", src=src, graph=csv_to_adjacency_list('hlgraph.csv'))
     except:
         return render_template("login_tobias.html", src=src, graph=csv_to_adjacency_list('hlgraph.csv'))
 
-@app.route("/van-<src>-naar-<dest_label>-<dest_number>-snelheid=<speed>-time=<time_input>")
-def map(src, dest_label, dest_number, speed, time_input):
+@app.route("/van-<src>/naar-<dest_label>/<dest_number>/snelheid=<speed>/time=<time_input>/type_tijd=<type_tijd>")
+def map(src, dest_label, dest_number, speed, time_input, type_tijd):
     # path = 'hlgraph.csv'
     # graph = csv_to_adjacency_list(path)
     # print(f"time_input: {time_input}, type: {type(time_input)}")
     graph = graph_for_time(convert_to_time(time_input))
     route, time = run_algorithm(graph=graph, startnode=int(src), endnode=int(dest_number), speed=float(speed))
-    ETA = (datetime.strptime(time_input, "%H:%M:%S") + timedelta(seconds=time)).strftime("%H:%M:%S")
+    ETA_or_PTL = (datetime.strptime(time_input, "%H:%M:%S") + timedelta(seconds=time)).strftime("%H:%M:%S")
     time_str = f"{int(time // 60)} minuten en {round(time % 60)} seconden"
-    return render_template("map.html", src=src, dest_label=dest_label, dest_number=dest_number, speed=speed, route=route, time=time_str, graph=graph, ETA=ETA)
+    return render_template("map.html", src=src, dest_label=dest_label, dest_number=dest_number, speed=speed, route=route, time=time_str, graph=graph, ETA_or_PTL = ETA_or_PTL)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))  # Default to port 5001
